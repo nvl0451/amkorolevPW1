69d7e59bd6397f0299baad6b788546d533733edb
korolev solutions <neoevil25@gmail.com>
2021-10-02 18:07:07 +0300

    8 points: amogus character out of 6 shapes, round corners, random color button, animation, button disable/enable

--- amkorolevPW1/ViewController.swift
+++ amkorolevPW1/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var views: [UIView]!
+    
+    @IBAction func changeColorTapped(_ sender: Any) {
+        let button = sender as? UIButton
+        button?.isEnabled = false
+        
+        var colorSet = Set<UIColor>()
+        while colorSet.count < views.count {
+            colorSet.insert(
+                    UIColor(
+                        red: .random(in: 0...1),
+                        green: .random(in: 0...1),
+                        blue: .random(in: 0...1),
+                        alpha: 1
+                    )
+            )
+        }
+        
+        
+        UIView.animate(withDuration: 1, animations: {
+            for view in self.views {
+                view.backgroundColor = colorSet.popFirst()
+            }
+        }, completion: {_ in button?.isEnabled = true})
+    }
+    
 }
 
